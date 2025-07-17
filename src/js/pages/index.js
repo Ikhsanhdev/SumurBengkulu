584638a26974eba1ece6bfb4e06bd0e7b1f25faf
IkhsanHadi <ikhsanhdn747@gmail.com>
2025-07-17 10:10:37 +0700

    update

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -48,6 +48,44 @@ function initMap() {
         ]
     });
 
+    function loadDAS(url, color) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.features.forEach(f => {
+                    if (!f.properties) f.properties = {};
+                    f.properties.fillColor = color;
+                });
+
+                map.data.addGeoJson(data);
+            });
+    }
+
+    loadDAS('/data/006_DAS_Bengkulu.json', 'red');
+    loadDAS('/data/005_WS_Nasal-Padang_Guci.json', 'blue');
+    loadDAS('/data/004_WS_Bengkulu-Alas-Talo.json', 'yellow');
+    loadDAS('/data/003_WS_Sebelat-Ketahun-Lais.json', 'red');
+    loadDAS('/data/002_WS_Teramang-Muar.json', 'orange');
+    loadDAS('/data/001_WS_Silaut-Tarusan.json', 'green');
+
+    map.data.setStyle(function(feature) {
+        const fillColor = feature.getProperty('fillColor') || 'gray';
+        return {
+            fillColor: fillColor,
+            fillOpacity: 0.3,
+            strokeColor: 'white',
+            strokeWeight: 0.7
+        };
+    });
+
+    map.data.addListener('click', function(event) {
+        const name = event.feature.getProperty('name');
+        new google.maps.InfoWindow({
+            content: `<strong>${name}</strong>`,
+            position: event.latLng
+        }).open(map);
+    });
+
     const geocoder = new google.maps.Geocoder();
     geocoder.geocode({ location: jakarta }, (results, status) => {
         if (status === "OK" && results[0]) {
@@ -66,3 +104,44 @@ function initMap() {
         }
     });
 }
+
+$(document).ready(function() {
+    $("#table-ews").DataTable({ 
+        responsive: false,
+        language: { 
+            paginate: { 
+                previous: "<i class='mdi mdi-chevron-left'>", 
+                next: "<i class='mdi mdi-chevron-right'>" 
+            } 
+        }, 
+        drawCallback: function () { 
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded") 
+        }
+    });
+
+    $("#table-rainfall").DataTable({ 
+        responsive: false,
+        language: { 
+            paginate: { 
+                previous: "<i class='mdi mdi-chevron-left'>", 
+                next: "<i class='mdi mdi-chevron-right'>" 
+            } 
+        }, 
+        drawCallback: function () { 
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded") 
+        }
+    });
+
+    $("#table-tma").DataTable({ 
+        responsive: false,
+        language: { 
+            paginate: { 
+                previous: "<i class='mdi mdi-chevron-left'>", 
+                next: "<i class='mdi mdi-chevron-right'>" 
+            } 
+        }, 
+        drawCallback: function () { 
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded") 
+        }
+    });
+});
